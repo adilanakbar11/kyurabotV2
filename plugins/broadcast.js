3cb9021f7702494ab5ba546be8812e4d202eccbe
Kyura <95233385+KyuraSaja@users.noreply.github.com>
2022-01-03 18:15:56 +0800

    berodkes berwaktu biar ga kena ban

--- plugins/broadcast.js
+++ plugins/broadcast.js
@@ -1,28 +1,18 @@
-let handler  = async (m, { conn, text }) => {
-  let chats = conn.chats.all().filter(v => !v.read_only && v.message && !v.archive).map(v => v.jid)
+let handler = async (m, { conn, text }) => {
+  let chats = conn.chats.all().filter(v => v.jid.endsWith('.net')).map(v => v.jid).filter(v => !v.startsWith(owner[0]))
   let cc = conn.serializeM(text ? m : m.quoted ? await m.getQuotedObj() : false || m)
   let teks = text ? text : cc.text
-  conn.reply(m.chat, `_Mengirim pesan broadcast ke ${chats.length} chat_`, m)
-  for (let id of chats) await conn.copyNForward(id, conn.cMod(m.chat, cc, /bc|broadcast/i.test(teks) ? teks : teks + '\n' + readMore + `\n「 ${bc} Broadcast 」`), true).catch(_=>_)
-  m.reply('Selesai Broadcast All Chat :)')
+  conn.reply(m.chat, `Mengirim pesan broadcast ke ${chats.length} chat\nEstimasi selesai ${chats.length * 1.5} detik`, m)
+  for (let id of chats) {
+    await conn.delay(2000)
+    await conn.copyNForward(id, conn.cMod(m.chat, cc, /bc|broadcast/i.test(teks) ? teks : '「 *Broadcast* 」\n\n' + teks + '\n\n- kyura'), true).catch(_ => _)
+  }
+  m.reply('done bc , kyura kun')
 }
-handler.help = ['broadcast','bc'].map(v => v + ' <teks>')
+handler.help = ['broadcast', 'bc'].map(v => v + ' <teks>')
 handler.tags = ['owner']
 handler.command = /^(broadcast|bc)$/i
+
 handler.owner = true
-handler.mods = false
-handler.premium = false
-handler.group = false
-handler.private = false
 
-handler.admin = false
-handler.botAdmin = false
-
-handler.fail = null
-
-module.exports = handler
-
-const more = String.fromCharCode(8206)
-const readMore = more.repeat(4001)
-
-const randomID = length => require('crypto').randomBytes(Math.ceil(length * .5)).toString('hex').slice(0, length)
+module.exports = handler 
